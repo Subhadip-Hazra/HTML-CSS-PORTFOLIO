de4d133dd0fc78f98fd599a70103c4ed24e60236
Subhadip Hazra <146234744+Subhadip-Hazra@users.noreply.github.com>
2023-10-09 22:51:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.setProperty('--primary-color','#ee11d3');
+        body.style.setProperty('--secondary-color','#04c1f5a9');
+        body.style.setProperty('--background-color', '#030327fb');
+        body.style.setProperty('--next-background-color','#2b03f43c');
+        body.style.setProperty('--text-color-light','#f7f3f3');
+        body.style.setProperty('--text-color-dark','#0000009f;');
+        body.style.setProperty('--accent-color','#057ff2;');
+        body.style.setProperty('--box-shadow-color','rgba(0, 0, 0, 0.25)');
+        body.style.transition = '2s';
+    }else{
+        body.style.setProperty('--primary-color','#6c33d6a5');
+        body.style.setProperty('--secondary-color','#f504bda9');
+        body.style.setProperty('--background-color', '#c5f2f7');
+        body.style.setProperty('--next-background-color','#2b03f43c');
+        body.style.setProperty('--text-color-light','#000000');
+        body.style.setProperty('--text-color-dark','#fcfafa9f;');
+        body.style.setProperty('--accent-color','#057ff2;');
+        body.style.setProperty('--box-shadow-color','rgba(5, 5, 247, 0.148);');
+        body.style.transition = '2s';
+        
+    }
+});
